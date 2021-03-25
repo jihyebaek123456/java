2d690718d6c4583e37b0aa557e5e8b24077af5e4
DESKTOP-MLAJ9CK\PC-15 <bjh28712745@gmail.com>
2021-03-25 16:17:14 +0900

    0325 메소드 계산기

---
+++ e_oop/Calculator.java
@@ -0,0 +1,27 @@
+package e_oop;
+
+public class Calculator {
+	
+	double add (double num1, double num2) {
+		double result = num1 + num2;
+		return result;
+	}
+	double subtract (double num1, double num2) {
+		double result = num1 - num2;
+		return result;
+	}
+	double multiply (double num1, double num2) {
+		double result = num1 * num2;
+		return result;
+	}
+	double divide (double num1, double num2) {
+		double result = num1 / num2;
+		return result;
+	}
+	double mod (double num1, double num2) {
+		double result = num1 % num2;
+		return result;
+	}
+	
+	
+}
