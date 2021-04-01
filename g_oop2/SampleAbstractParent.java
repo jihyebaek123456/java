e87b7c483e090b53af25a859e8b4bf4f252639f2
DESKTOP-MLAJ9CK\PC-15 <bjh28712745@gmail.com>
2021-04-01 12:48:08 +0900

    0330 Java 추상 메소드

---
+++ g_oop2/SampleAbstractParent.java
@@ -0,0 +1,14 @@
+package g_oop2;
+
+public abstract class SampleAbstractParent {
+	abstract void abstractMethod();
+}
+
+class SampleAbstractChild extends SampleAbstractParent {
+	
+	@Override
+	void abstractMethod() {
+		
+	}
+	
+}
