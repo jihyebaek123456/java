db40ce56a3b1ca0efdf230e86f87fc485d5ba829
DESKTOP-MLAJ9CK\PC-15 <bjh28712745@gmail.com>
2021-04-09 17:17:15 +0900

    JDBC 게시판 만들기

---
+++ k_jdbc/JDBCBoard.java
@@ -0,0 +1,8 @@
+package k_jdbc;
+
+public class JDBCBoard {
+	public static void main(String[] args) {
+		//오라클 데이터베이스에 생성한 게시판 테이블을 이용해 게시판 프로그램 만들기
+		//BOARD_NO(번호, 기본키), TITLE(제목), CONTENT(내용), USER_ID(작성자), REG_DATE(작성일)
+	}
+}
